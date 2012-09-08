d571b1c0668b1365058c07a58efb33a2e7900912
Nick Fraioli <nfraioli@Nicks-MacBook-Pro.local>
2012-09-08 14:55:35 -0700

    First assignment for Class1

---
+++ Class1/subtract.c
@@ -0,0 +1,29 @@
+//
+//	class1.c
+//	EmilyClass
+//
+//	Created by Nick Fraioli on 9/8/12.
+//	Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char **argv) {
+	
+	if (argc != 3) {
+		fprintf(stderr, "Usage: subtract <digit1> <digit2>\n");
+		return 1;
+	}
+	
+	char firstNumber = argv[1][0];
+	char secondNumber = argv[2][0];
+	int difference = 0;
+	
+	/*
+	 Write code here. Take the difference of the first and second number (firstNumber - secondNumber),
+	 and set 'difference' to that value.
+	*/
+
+	printf("The difference is %d\n", difference);
+	return 0;
+}
