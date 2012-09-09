f441995d92d15111cd451d382a1e33ddba54afc0
Emily Janzer <emily.janzer@gmail.com>
2012-09-09 01:34:36 -0700

    Solution to bitmask assignment from Class2

--- Class2/bitmask.c
+++ Class2/bitmask.c
@@ -8,7 +8,28 @@
     1000
 */
 int and(int a, int b) {
-	return 0;
+    int i = 0;
+    int tempNumber = 0;
+    int answer;
+    
+    while (a > 0 || b > 0) {
+        if ((a % 2 == 1) && (b % 2 == 1)) {
+            tempNumber = 1;
+        } else {
+            tempNumber = 0;
+        }
+        int base = 1;
+        int j = 0;
+        while (j < i) {
+            base = base * 2;
+            j++;
+        }
+        answer += tempNumber * base;
+        a = a/2;
+        b = b/2;
+        i++;
+    }
+	return answer;
 }
 
 /* Bitwise OR (|) requires either bit to be 1 to result in a 1:
@@ -18,7 +39,28 @@ int and(int a, int b) {
     1101
 */
 int or(int a, int b) {
-	return 0;
+    int answer = 0;
+    int i = 0;
+    int tempNumber = 0;
+    while (a > 0 || b > 0) {
+        if ((a % 2 == 1) || (b % 2 == 1)) {
+            tempNumber = 1;
+        } else {
+            tempNumber = 0;
+        }
+        int base = 1;
+        int j = 0;
+        while (j < i) {
+            base = base * 2;
+            j++;
+        }
+        answer += tempNumber * base;
+        a = a/2;
+        b = b/2;
+        i++;
+    }
+    
+	return answer;
 }
 
 /* Bitwise XOR (^) or "exclusive or" requires both bits to be different to result in a 1:
@@ -28,7 +70,27 @@ int or(int a, int b) {
     0101
 */
 int xor(int a, int b) {
-	return 0;	
+    int answer = 0;
+    int i = 0;
+    int tempNumber = 0;
+    while (a > 0 || b > 0) {
+        if ((a % 2) + (b % 2) == 1) {
+            tempNumber = 1;
+        } else {
+            tempNumber = 0;
+        }
+        int j = 0;
+        int base = 1;
+        while (j < i) {
+            base = base * 2;
+            j++;
+        }        
+        answer += tempNumber * base;
+        a = a/2;
+        b = b/2;
+        i++;
+    }
+	return answer;
 }
 
 int main(int argc, const char **argv) {
